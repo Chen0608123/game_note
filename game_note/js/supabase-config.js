2afaf17ed35ea3ee914a18fce064910b818fdcfb
Wei-Zhe <s111213230@stu.ntue.edu.tw>
2026-07-01 17:03:25 +0800

    Delete game_note directory

--- game_note/js/supabase-config.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const SUPABASE_URL = "https://oksiosigamvornwjtnnf.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_117UIXXAvXlNde0JKTgYeA_coQjlc8L";
-export const MEDIA_BUCKET = "game-media";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
